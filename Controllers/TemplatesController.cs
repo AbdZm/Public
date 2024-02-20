c434bc2ea31301290f88101c07b941a1c416e66e
AbdZm <87931897+AbdZm@users.noreply.github.com>
2024-02-20 23:45:32 +0300

    Add files via upload

---
+++ Controllers/TemplatesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OCRProject.Data;
+using OCRProject.Models;
+
+namespace OCRProject.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TemplatesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Templates
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Template.Include(t => t.Att).Include(t => t.Cat);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Templates/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var template = await _context.Template
+                .Include(t => t.Att)
+                .Include(t => t.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return View(template);
+        }
+
+        // GET: Templates/Create
+        public IActionResult Create()
+        {
+            ViewData["AttributeOCRId"] = new SelectList(_context.AttributeOCR, "Id", "Id");
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id");
+            return View();
+        }
+
+        // POST: Templates/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CategoryId,AttributeOCRId,X,Y,Height,Width,Note")] Template template)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(template);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AttributeOCRId"] = new SelectList(_context.AttributeOCR, "Id", "Id", template.AttributeOCRId);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", template.CategoryId);
+            return View(template);
+        }
+
+        // GET: Templates/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var template = await _context.Template.FindAsync(id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+            ViewData["AttributeOCRId"] = new SelectList(_context.AttributeOCR, "Id", "Id", template.AttributeOCRId);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", template.CategoryId);
+            return View(template);
+        }
+
+        // POST: Templates/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CategoryId,AttributeOCRId,X,Y,Height,Width,Note")] Template template)
+        {
+            if (id != template.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(template);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TemplateExists(template.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AttributeOCRId"] = new SelectList(_context.AttributeOCR, "Id", "Id", template.AttributeOCRId);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", template.CategoryId);
+            return View(template);
+        }
+
+        // GET: Templates/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var template = await _context.Template
+                .Include(t => t.Att)
+                .Include(t => t.Cat)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return View(template);
+        }
+
+        // POST: Templates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var template = await _context.Template.FindAsync(id);
+            _context.Template.Remove(template);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TemplateExists(int id)
+        {
+            return _context.Template.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OCRProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
